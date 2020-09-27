5132f2b4f333e29ddf1b2befc92c8c8bfe9b4579
Danilo Vesovic <vesovicdanilo@gmail.com>
2020-09-27 17:57:43 +0200

    question added

--- question.js
+++ question.js
@@ -12,5 +12,7 @@ const questions = [
     new Question("Koji je drugi najveci grad u Srbiji posle Beograda ?","nis",['Berlin','Beograd','Nis','Novi Sad'],'Novi Sad'),
     new Question("Izbaci uljeza","nis",['Kokoska','Pingvin','Noj','Orao'],'Orao'),
     new Question("Koliko hobotnica ima pipaka ?","nis",['8','4','6','3'],'8'),
-    new Question("Najveca zivotinja na svetu je ?","nis",['Kit','Slon','Medved','Krokodil'],'Kit')
+    new Question("Najveca zivotinja na svetu je ?","nis",['Kit','Slon','Medved','Krokodil'],'Kit'),
+    new Question("Kako se zvala plava riba iz crtanog Nemo","nis",['Klara','Dora','Mery','Sara'],'Dora'),
+    new Question("Sofija ide kod ujaka, on je","nis",['Mamin brat','Tatin brat','Mamina sestra','Tatina sestra'],'Mamin brat'),
 ];
